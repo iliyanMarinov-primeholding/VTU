538d482e92f77a3d5f9754f7d83be13034f6dfc3
ILIYAN MARINOV <iliyan.marinov@sbnd.net>
2019-11-18 17:40:01 +0200

    check constraint

--- RDBMS/dbo/Tables/dbo.PhoneInfo.sql
+++ RDBMS/dbo/Tables/dbo.PhoneInfo.sql
@@ -3,8 +3,9 @@
     [PersonalInfoId] UNIQUEIDENTIFIER NOT NULL,
     [AreaCode]       NVARCHAR (64)    NOT NULL,
     [PhonenNumber]   NVARCHAR (128)   NOT NULL,
-    [ext]            NVARCHAR (64)    NULL,
+    [ext]            NVARCHAR (64)    NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [CK_PhoneInfo_Phone_Len] CHECK (len(Trim((' ,.<>/?;'':"[]\{}|`~!@#$%^&*()_+-='+char((10)))+char((13)) FROM [PhonenNumber]))>(0)),
     CONSTRAINT [FK_PhoneInfo_PersonalInfo] FOREIGN KEY ([PersonalInfoId]) REFERENCES [dbo].[PersonalInfo] ([Id])
 );
 
